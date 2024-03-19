DataDog/kong-plugin-ddtrace

commit 3bd8fa57fdb27fb7ff96743fa0ea9b14e809bb95

feat: support 128-bit trace ID (#33)
feat: support 128-bit trace ID

- Generate 128-bit trace ids by default. This can be override with the
  new `generate_128bit_trace_ids` option.
- Propagation: inject datadog internal tags to HTTP Headers.
- Propagation: extract datadog internal tags from HTTP Headers.
- Add unit tests.

kong/plugins/ddtrace/agent_writer.lua
@@ -33,10 +33,34 @@ local function new(conf, sampler, tracer_version)
     }, agent_writer_mt)
 end
 
+local function encode_span(span)
+    return encoder.pack({
+        type = span.type,
+        service = span.service,
+        name = span.name,
+        resource = span.resource,
+        trace_id = span.trace_id.low,
+        span_id = span.span_id,
+        parent_id = span.parent_id,
+        start = span.start,
+        sampling_priority = span.sampling_priority,
+        origin = span.origin,
+        meta = span.meta,
+        metrics = span.metrics,
+        error = span.error,
+    })
+end
+
 
 function agent_writer_methods:add(spans)
     local i = self.trace_segments_n + 1
-    self.trace_segments[i] = encoder.pack(spans)
+
+    local buffer = encoder.arrayheader(#spans)
+    for _, span in ipairs(spans) do
+        buffer = buffer .. encode_span(span)
+    end
+
+    self.trace_segments[i] = buffer
     self.trace_segments_n = i
 end
 

kong/plugins/ddtrace/datadog_propagation.lua
@@ -12,15 +12,15 @@ local function id_to_string(id)
   return string.sub(str_id, 1, #str_id - 3)
 end
 
-local function parse_uint64(str)
+local function parse_uint64(str, base)
     if not str then
         return nil, "unable to parse, value is nil"
     end
     ffi.errno(0)
-    local parsed_str = ffi.C.strtoull(str, nil, 10)
+    local parsed_str = ffi.C.strtoull(str, nil, base)
     local err = ffi.errno()
     if err ~= 0 then
-        return nil, "unable to parse '" .. str .. "' as 64-bit number, errno=" .. err
+        return nil, "unable to parse '" .. str .. "' (base " .. base .. ") as 64-bit number, errno=" .. err
     end
     -- TODO: check the entire string was consumed, instead of partially decoded
     return parsed_str, nil
@@ -67,14 +67,15 @@ local function extract_datadog(get_header, max_header_size)
         -- no trace ID found, therefore create a new span
         return nil, nil, nil, nil, nil, nil, nil
     end
-    local trace_id, err = parse_uint64(trace_id_value)
+    local trace_id_low, err = parse_uint64(trace_id_value, 10)
     if err then
         -- tracing was desired but the value wasn't understood
         return nil, nil, nil, nil, nil, nil, err
     end
 
     -- other headers are expected but aren't provided in all cases
-    local parent_id = parse_uint64(get_header("x-datadog-parent-id"))
+    local trace_id = {high=nil, low=trace_id_low}
+    local parent_id = parse_uint64(get_header("x-datadog-parent-id"), 10)
     local sampling_priority = tonumber(get_header("x-datadog-sampling-priority"))
     local origin = get_header("x-datadog-origin")
     local dd_tags = {}
@@ -87,6 +88,22 @@ local function extract_datadog(get_header, max_header_size)
               kong.log.warn("`x-datadog-tags` exceed the limit of " .. max_header_size .. " characters")
             else
               dd_tags = parse_dd_tags(dd_tags_value)
+            end
+        end
+    end
+
+    local tid = dd_tags["_dd.p.tid"]
+    if tid then
+        if #tid ~= 16 then
+            dd_tags["_dd.propagation_error"] = "malformed_tid " .. tid
+            dd_tags["_dd.p.tid"] = nil
+        else
+            local trace_id_high, err = parse_uint64(tid, 16)
+            if err then
+                dd_tags["_dd.propagation_error"] = "malformed_tid " .. tid
+                dd_tags["_dd.p.tid"] = nil
+            else
+                trace_id.high = trace_id_high
             end
         end
     end

spec/06_datadog_propagation_spec.lua
@@ -35,7 +35,9 @@ describe("trace propagation", function()
 
             local trace_id, parent_id, sampling_priority, origin, tags, err = extract_datadog(get_header, 512)
             assert.is_nil(err)
-            assert.equal(trace_id, 12345678901234567890ULL)
+
+            local expected_trace_id = {high=nil, low=12345678901234567890ULL}
+            assert.same(expected_trace_id, trace_id)
             assert.equal(parent_id, 9876543210987654321ULL)
             assert.equal(sampling_priority, 1)
             assert.same(origin, "test-origin")
@@ -45,6 +47,27 @@ describe("trace propagation", function()
             }
 
             assert.same(expected_tags, tags)
+        end)
+        it("extracts 128bit trace", function()
+            local headers = {
+                ["x-datadog-trace-id"] = "12345678901234567890",
+                ["x-datadog-parent-id"] = "9876543210987654321",
+                ["x-datadog-sampling-priority"] = "1",
+                ["x-datadog-origin"] = "test-origin",
+                ["x-datadog-tags"] = "_dd.p.tid=cbae7cb600000000,_dd.p.dm=-0"
+            }
+            local get_header = make_getter(headers)
+
+            local trace_id, parent_id, sampling_priority, origin, tags, err = extract_datadog(get_header, 512)
+            assert.is_nil(err)
+
+            local expected_trace_id = {high=14676805356772917248ULL, low=12345678901234567890ULL}
+            assert.same(trace_id, expected_trace_id)
+            assert.equal(parent_id, 9876543210987654321ULL)
+            assert.equal(sampling_priority, 1)
+            assert.same(origin, "test-origin")
+            assert.equal("cbae7cb600000000", tags["_dd.p.tid"])
+            assert.equal("-0", tags["_dd.p.dm"])
         end)
 
         describe("datadog tags extraction", function()
@@ -125,12 +148,38 @@ describe("trace propagation", function()
 
             local start_time = 1700000000000000000LL
             local duration = 100000000LL
-            local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil)
+            local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
             inject_datadog(span, set_header, 512)
             span:finish(start_time + duration)
             assert.equal(2, headers_set)
             assert.is_string(headers["x-datadog-trace-id"])
             assert.is_string(headers["x-datadog-parent-id"])
+        end)
+        it("injects 128 bit trace id", function()
+            -- add kong.service.request.set_header method
+            local headers = {}
+            local headers_set = 0
+            local set_header = function(key, value)
+                headers[key] = value
+                headers_set = headers_set + 1
+            end
+            _G.kong = {
+                service = {
+                    request = {
+                        set_header = set_header,
+                    },
+                },
+            }
+            local start_time = 1700000000000000000LL
+            local duration = 100000000LL
+            local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, true, nil)
+            inject_datadog(span, set_header, 512)
+            span:finish(start_time + duration)
+            assert.equal(headers_set, 3)
+            assert.is_string(headers["x-datadog-trace-id"])
+            assert.is_string(headers["x-datadog-parent-id"])
+            assert.is_string(headers["x-datadog-tags"])
+            assert.equal(headers["x-datadog-tags"], "_dd.p.tid=" .. bit.tohex(span.trace_id.high))
         end)
 
         describe("datadog tags injection", function()
@@ -150,7 +199,7 @@ describe("trace propagation", function()
                       }
 
                       local max_header_size = 0
-                      local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                      local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
                       span:set_tags(propagation_tags)
                       inject_datadog(span, set_header, max_header_size)
 
@@ -164,7 +213,7 @@ describe("trace propagation", function()
                       }
 
                       local max_header_size = 1
-                      local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                      local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
                       span:set_tags(propagation_tags)
                       inject_datadog(span, set_header, max_header_size)
 
@@ -175,7 +224,7 @@ describe("trace propagation", function()
             end)
 
             it("empty propagation tags", function()
-                  local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                  local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
                   inject_datadog(span, set_header, 512)
 
                   assert.is_nil(headers["x-datadog-tags"])
@@ -187,7 +236,7 @@ describe("trace propagation", function()
                     ["_dd.p.hello"] = "world",
                 }
 
-                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
                 span:set_tags(propagation_tags)
                 inject_datadog(span, set_header, 512)
 
@@ -204,7 +253,7 @@ describe("trace propagation", function()
                     ["_dd.p.hello"] = "mars",
                 }
 
-                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
                 span:set_tags(propagation_tags)
 
                 local child_start = start_time + 10
@@ -221,7 +270,7 @@ describe("trace propagation", function()
             it("sampling decision is propagated", function()
                 local new_sampler = require "kong.plugins.ddtrace.sampler".new
                 local sampler = new_sampler(10, nil)
-                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, nil)
+                local span = new_span("test_service", "test_name", "test_resource", nil, nil, nil, start_time, nil, nil, false, nil)
 
                 local ok = sampler:sample(span)
                 assert.is_true(ok)

kong/plugins/ddtrace/handler.lua
@@ -209,7 +209,8 @@ if subsystem == "http" then
             name = "kong.plugin.ddtrace",
             start_us = ngx.ctx.KONG_PROCESSING_START * 1000000LL,
             -- TODO: decrease cardinality of path value
-            resource = method .. " " .. apply_resource_name_rules(path, conf.resource_name_rule)
+            resource = method .. " " .. apply_resource_name_rules(path, conf.resource_name_rule),
+            generate_128bit_trace_ids = conf.generate_128bit_trace_ids
         }
 
         local request_span = propagator.extract_or_create_span(req, span_options, conf.max_header_size)

kong/plugins/ddtrace/propagation.lua
@@ -3,7 +3,7 @@ local new_span = require "kong.plugins.ddtrace.span".new
 
 local function extract_or_create_span(request, span_options, max_header_size)
     local trace_id, parent_id, sampling_priority, origin, propagation_tags, err = datadog.extract(request.get_header, max_header_size)
-    local span = new_span(span_options.service, span_options.name, span_options.resource, trace_id, nil, parent_id, span_options.start_us, sampling_priority, origin, nil)
+    local span = new_span(span_options.service, span_options.name, span_options.resource, trace_id, nil, parent_id, span_options.start_us, sampling_priority, origin, span_options.generate_128bit_trace_ids, nil)
     if propagation_tags then
         span:set_tags(propagation_tags)
     end

kong/plugins/ddtrace/sampler.lua
@@ -79,7 +79,7 @@ end
 -- returns whether the span is sampled based on the max_id
 local function sampling_decision(span, max_id)
     -- not-ideal knuth hashing of trace ids
-    local hashed_trace_id = span.trace_id * 1111111111111111111ULL
+    local hashed_trace_id = span.trace_id.low * 1111111111111111111ULL
     if hashed_trace_id > max_id then
         return false
     end

kong/plugins/ddtrace/schema.lua
@@ -120,6 +120,7 @@ return {
                 { version = allow_referenceable({ type = "string", default = "{vault://env/dd-version}" }, nil) },
                 { header_tags = { type = "array", elements = header_tag, custom_validator = validate_header_tag } },
                 { max_header_size = { type = "integer", default = 512, between = {0, 512} } },
+                { generate_128bit_trace_ids = { type = "boolean", default = true } },
             },
         }, },
     },

kong/plugins/ddtrace/span.lua
@@ -18,34 +18,59 @@ local span_mt = {
 local uint64_t = ffi.typeof("uint64_t")
 local int64_t = ffi.typeof("int64_t")
 
-local function generate_span_id()
+local function random_64bit()
     local x = rand_bytes(8)
     local b = {1ULL * byte(x,1), 1ULL * byte(x,2), 1ULL * byte(x,3), 1ULL * byte(x,4), 1ULL * byte(x,5), 1ULL * byte(x,6), 1ULL * byte(x,7), 1ULL * byte(x,8)}
     local id = bit.bor(bit.lshift(b[1], 56), bit.lshift(b[2], 48), bit.lshift(b[3], 40), bit.lshift(b[4], 32), bit.lshift(b[5], 24), bit.lshift(b[6], 16), bit.lshift(b[7], 8), b[8])
-    -- truncate to 63-bit value
-    return bit.band(id, 0x7FFFFFFFFFFFFFFFULL)
+    return id
+end
+
+local function legacy_63bit_ids_generator()
+    -- NOTE(@dmehala): truncate to 63-bit value for legacy reasons
+    return bit.band(random_64bit(), 0x7FFFFFFFFFFFFFFFULL)
+end
+
+local function trace_64bit_ids_generator(_)
+    return {high=nil, low=legacy_63bit_ids_generator()}
+end
+
+local function trace_128bit_ids_generator(now_us)
+    local now_s = uint64_t(now_us / 1000000)
+    local msb = bit.lshift(now_s, 32)
+    return {high=msb, low=random_64bit()}
 end
 
 local function new(service, name, resource,
     trace_id, span_id, parent_id,
-    start, sampling_priority, origin, root)
+    start_us, sampling_priority, origin, generate_128bit_trace_ids, root)
     assert(type(name) == "string" and name ~= "", "invalid span name")
     assert(type(resource) == "string" and resource ~= "", "invalid span resource")
-    assert(trace_id == nil or ffi.istype(uint64_t, trace_id), "invalid trace id")
+    assert(trace_id == nil or type(trace_id) == "table", "invalid trace id")
     assert(span_id == nil or ffi.istype(uint64_t, span_id), "invalid span id")
     assert(parent_id == nil or ffi.istype(uint64_t, parent_id), "invalid parent id")
-    assert(ffi.istype(int64_t, start) and start >= 0, "invalid span start timestamp")
+    assert(ffi.istype(int64_t, start_us) and start_us >= 0, "invalid span start timestamp")
     assert(sampling_priority == nil or type(sampling_priority) == "number", "invalid sampling priority")
     assert(root == nil or type(root) == "table", "invalid root span")
+    assert(type(generate_128bit_trace_ids) == "boolean")
+
+    local trace_id_generator = (generate_128bit_trace_ids and trace_128bit_ids_generator) or trace_64bit_ids_generator
 
     if trace_id == nil then
         -- a new trace
-        trace_id = generate_span_id()
-        span_id = trace_id
+        trace_id = trace_id_generator(start_us)
+        span_id = trace_id.low
         parent_id = uint64_t(0)
     elseif span_id == nil then
         -- a new span for an existing trace
-        span_id = generate_span_id()
+        span_id = legacy_63bit_ids_generator()
+    end
+
+    local meta = {
+      language = "lua"
+    }
+
+    if root == nil and trace_id.high ~= nil then
+        meta["_dd.p.tid"] = bit.tohex(trace_id.high)
     end
 
     return setmetatable({
@@ -56,17 +81,16 @@ local function new(service, name, resource,
         trace_id = trace_id,
         span_id = span_id,
         parent_id = parent_id,
-        start = start,
+        start = start_us,
         sampling_priority = sampling_priority,
         origin = origin,
-        meta = {
-            ["language"] = "lua"
-        },
+        meta = meta,
         metrics = {
             ["_sampling_priority_v1"] = sampling_priority,
         },
         error = 0,
         root = root,
+        generate_128bit_trace_ids = generate_128bit_trace_ids,
     }, span_mt)
 end
 
@@ -89,11 +113,12 @@ function span_methods:new_child(name, resource, start)
         name,
         resource,
         self.trace_id,
-        generate_span_id(),
+        legacy_63bit_ids_generator(),
         self.span_id,
         start,
         self.sampling_priority,
         self.origin,
+        self.generate_128bit_trace_ids,
         self.root or self
     )
 end
@@ -110,15 +135,11 @@ end
 
 
 function span_methods:set_tag(key, value)
-    -- kong.log.err(fmt("set_tag: '%s': '%s' (%s)", key, value, type(value)))
     assert(type(key) == "string", "invalid tag key")
     if value ~= nil then -- Validate value
         local vt = type(value)
         assert(vt == "string" or vt == "number" or vt == "boolean",
         "invalid tag value (expected string, number, boolean or nil)")
-    end
-    if not self.meta then
-        self.meta = {}
     end
     if value then
         self.meta[key] = tostring(value)
@@ -127,14 +148,6 @@ function span_methods:set_tag(key, value)
     end
     return true
 end
-
-
-function span_methods:each_tag()
-    local tags = self.tags
-    if tags == nil then return function() end end
-    return next, tags
-end
-
 
 function span_methods:set_http_header_tags(header_tags, get_request_header, get_response_header)
     for header_name, tag_entry in pairs(header_tags) do

commit 5544485d435ff66d18a80bbdd6b25fd322dad64b

fix: HTTP injection and span encoding
Fix a several regressions introduced in #33:
- Send the duration of a span to the agent.
- Inject the lower 64bit of trace ID instead of the whole table.
- Update unit tests.

kong/plugins/ddtrace/agent_writer.lua
@@ -29,6 +29,7 @@ local function encode_span(span)
         span_id = span.span_id,
         parent_id = span.parent_id,
         start = span.start,
+        duration = span.duration,
         sampling_priority = span.sampling_priority,
         origin = span.origin,
         meta = span.meta,

kong/plugins/ddtrace/datadog_propagation.lua
@@ -119,7 +119,7 @@ local function inject_datadog(span, set_header, max_header_size)
         return "unable to inject: span's trace_id is nil"
     end
 
-    set_header("x-datadog-trace-id", id_to_string(span.trace_id))
+    set_header("x-datadog-trace-id", id_to_string(span.trace_id.low))
     set_header("x-datadog-parent-id", id_to_string(span.span_id))
 
     if span.sampling_priority then

spec/06_datadog_propagation_spec.lua
@@ -10,6 +10,13 @@ local function make_getter(headers)
     return getter
 end
 
+local function id_to_string(id)
+    -- when concerted to a string, uint64_t values have ULL at the end of the string.
+    -- string.sub is used to remove the last 3 characters.
+    local str_id = tostring(id)
+    return string.sub(str_id, 1, #str_id - 3)
+end
+
 _G.kong = {
     log = {
         warn = function(s) end,
@@ -154,24 +161,16 @@ describe("trace propagation", function()
             inject_datadog(span, set_header, 512)
             span:finish(start_time + duration)
             assert.equal(2, headers_set)
-            assert.is_string(headers["x-datadog-trace-id"])
-            assert.is_string(headers["x-datadog-parent-id"])
+            assert.equal(id_to_string(span.trace_id.low), headers["x-datadog-trace-id"])
+            assert.equal(id_to_string(span.span_id), headers["x-datadog-parent-id"])
         end)
         it("injects 128 bit trace id", function()
-            -- add kong.service.request.set_header method
             local headers = {}
             local headers_set = 0
             local set_header = function(key, value)
                 headers[key] = value
                 headers_set = headers_set + 1
             end
-            _G.kong = {
-                service = {
-                    request = {
-                        set_header = set_header,
-                    },
-                },
-            }
             local start_time = 1700000000000000000LL
             local duration = 100000000LL
             local span =
@@ -179,8 +178,8 @@ describe("trace propagation", function()
             inject_datadog(span, set_header, 512)
             span:finish(start_time + duration)
             assert.equal(headers_set, 3)
-            assert.is_string(headers["x-datadog-trace-id"])
-            assert.is_string(headers["x-datadog-parent-id"])
+            assert.equal(id_to_string(span.trace_id.low), headers["x-datadog-trace-id"])
+            assert.equal(id_to_string(span.span_id), headers["x-datadog-parent-id"])
             assert.is_string(headers["x-datadog-tags"])
             assert.equal(headers["x-datadog-tags"], "_dd.p.tid=" .. bit.tohex(span.trace_id.high))
         end)